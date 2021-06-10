724b6169d7bc090c9f51a58527f0b736f1b80a74
Jonathan Abrahams <jonathan.abrahams@xneelo.com>
2021-06-10 21:46:41 +0200

    Add config to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
+#include "config.h"
 #include <iostream>
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Welcome to the game of life" << std::endl;
+    std::cout << GameOfLife_DESCRIPTION << std::endl 
+              << "Homepage: " << GameOfLife_HOMEPAGE_URL << std::endl
+              << "Version: " << GameOfLife_VERSION << std::endl;
 
     return 0;
 }
